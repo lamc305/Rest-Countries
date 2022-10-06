d94298766eaf187bf3a49c9cd918cce009231862
Luis Medina <lamc3005@gmail.com>
2022-10-06 08:48:56 -0400

    add pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import Filters from "./components/Filters"
+import { HashRouter, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
-import ListOfCountries from "./components/ListOfCountries"
+import CountrySingle from "./Pages/CountrySingle.jsx"
+import Home from "./Pages/Home"
 
 function App() {
 
@@ -8,8 +9,12 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Filters />
-      <ListOfCountries />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/country/:name" element={<CountrySingle />} />
+        </Routes>
+      </HashRouter>
     </div>
   )
 }
